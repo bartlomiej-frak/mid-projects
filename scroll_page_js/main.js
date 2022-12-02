96dacb7e590bb65af4628e6b62da6838ab9a6267
Bartek <bartlomiej.fr1@gmail.com>
2022-12-02 21:08:55 +0100

    scroll_page_js added

---
+++ scroll_page_js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Hello world");
+
+    const rootElement = document.querySelector("#root");
+    const sections = document.querySelectorAll("section");
+    let currentSectionIndex = 0;
+    let isThrottled = false;
+
+    document.addEventListener("mousewheel", function (event) {
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(function () {
+            isThrottled = false;
+        }, 1000);
+
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const isFirstSection = currentSectionIndex === 0;
+            if (isFirstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
+});
