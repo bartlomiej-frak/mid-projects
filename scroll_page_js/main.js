dc95a802754918dcd7d2babd95d56501e9a4c43c
Bartek <bartlomiej.fr1@gmail.com>
2022-12-05 12:32:28 +0100

    scroll_page_js completed

--- scroll_page_js/main.js
+++ scroll_page_js/main.js
@@ -1,34 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Hello world");
+    const scroller = new Scroller("#root");
 
-    const rootElement = document.querySelector("#root");
-    const sections = document.querySelectorAll("section");
-    let currentSectionIndex = 0;
-    let isThrottled = false;
-
-    document.addEventListener("mousewheel", function (event) {
-        if (isThrottled) return;
-        isThrottled = true;
-
-        setTimeout(function () {
-            isThrottled = false;
-        }, 1000);
-
-        const direction = event.wheelDelta < 0 ? 1 : -1;
-
-        if (direction === 1) {
-            const isLastSection = currentSectionIndex === sections.length - 1;
-            if (isLastSection) return;
-        } else if (direction === -1) {
-            const isFirstSection = currentSectionIndex === 0;
-            if (isFirstSection) return;
+    document.addEventListener("wheel", (event) => scroller.listenScroll(event));
+    document.addEventListener("swipeUp", () => scroller.scroll(1));
+    document.addEventListener("swipeDown", () => scroller.scroll(-1));
+    document.addEventListener("keydown", (event) => {
+        switch (event.keyCode) {
+            case 40:
+                return scroller.scroll(1);
+            case 38:
+                return scroller.scroll(-1);
+            default:
+                return;
         }
-
-        currentSectionIndex = currentSectionIndex + direction;
-
-        sections[currentSectionIndex].scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-        });
     });
 });
