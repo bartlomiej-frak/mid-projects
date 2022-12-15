d903c637f5457ef9958db91c491ead3b789ab6de
Bartek <bartlomiej.fr1@gmail.com>
2022-12-15 13:42:37 +0100

    add table to layout with js

--- responsive_page_with_data_table/dist/js/main.js
+++ responsive_page_with_data_table/dist/js/main.js
@@ -15,7 +15,7 @@
   \********************/
 /***/ (function() {
 
-eval("console.log(\"hello world!\");\n\n//# sourceURL=webpack://webpack_project/./js/main.js?");
+eval("const values = [{\n  name: \"Pineapple\",\n  calories: 33,\n  fat: 0,\n  carbs: 11.8\n}, {\n  name: \"Apple\",\n  calories: 57,\n  fat: 0.7,\n  carbs: 12.1\n}, {\n  name: \"Orange\",\n  calories: 51,\n  fat: 0.2,\n  carbs: 11.3\n}, {\n  name: \"Cherry\",\n  calories: 67,\n  fat: 0.4,\n  carbs: 14.6\n}];\nconst valuesContainer = document.querySelector(\".app--values\");\nconst desktopViewport = window.matchMedia(\"screen and (min-width: 500px)\");\nconst drawDesktopValues = () => {\n  valuesContainer.innerHTML = \"\";\n  let table = document.createElement(\"table\");\n  let thead = document.createElement(\"thead\");\n  thead.innerHTML = \"<tr><th>Name</th><th>Calories</th><th>Fat</th><th>Carbohydrates</th></tr>\";\n  let tbody = document.createElement(\"tbody\");\n  values.forEach(value => {\n    let row = document.createElement(\"tr\");\n    row.innerHTML = `<td>${value.name}</td><td>${value.calories}</td><td>${value.fat}</td><td>${value.carbs}</td>`;\n    tbody.appendChild(row);\n  });\n  table.appendChild(thead);\n  table.appendChild(tbody);\n  valuesContainer.appendChild(table);\n};\ndrawDesktopValues();\n\n//# sourceURL=webpack://webpack_project/./js/main.js?");
 
 /***/ }),
 
