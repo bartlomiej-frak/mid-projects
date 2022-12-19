7959c967597517445366e3caa0c29043807368fe
Bartek <bartlomiej.fr1@gmail.com>
2022-12-19 21:46:13 +0100

    animation based on fps

--- stars_animation_canvas/js/main.js
+++ stars_animation_canvas/js/main.js
@@ -11,6 +11,7 @@ class Sky {
             isClosed: false,
             width: null,
         };
+        this.lastUpdate = 0;
     }
 
     initCanvas() {
@@ -48,8 +49,8 @@ class Sky {
 
     updateStars() {
         this.stars.forEach((star) => {
-            star.x += star.speed;
-            star.y -= (star.speed * (this.width / 2 - star.x)) / 3000;
+            star.x += star.speed * (this.delta / 16);
+            star.y -= (star.speed * (this.delta / 16) * (this.width / 2 - star.x)) / 3000;
             star.radius = star.originalRadius * (Math.random() / 4 + 0.9);
 
             if (star.x > this.width + 2 * star.radius) {
@@ -81,7 +82,7 @@ class Sky {
 
     updateConstellation() {
         if (this.constellation.width > 0) {
-            this.constellation.width -= 0.04;
+            this.constellation.width -= 0.04 * (this.delta / 16);
         } else this.constellation.width = 0;
     }
 
@@ -154,7 +155,9 @@ class Sky {
     }
 
     draw(now) {
+        this.delta = now - this.lastUpdate;
         this.clearCanvas();
+
         this.drawStars();
         this.updateStars();
 
@@ -169,13 +172,15 @@ class Sky {
             this.generateRandomConstellation();
         }
 
+        this.lastUpdate = now;
+
         window.requestAnimationFrame((now) => this.draw(now));
     }
 
     run() {
         this.initCanvas();
         this.generateStars(500);
-        this.draw();
+        this.draw(0);
     }
 }
 
