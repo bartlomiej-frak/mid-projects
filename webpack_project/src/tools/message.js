2242585d28a215771df1ab565a646163a9b22dba
Bartek <bartlomiej.fr1@gmail.com>
2023-03-06 22:00:08 +0100

    testing google maps

--- webpack_project/src/tools/message.js
+++
@@ -1,5 +0,0 @@
-export const message = (info) => console.log(info);
-export const messageDOM = (info) => {
-    const container = document.querySelector("div");
-    container.textContent = info;
-};
