1894a58b79031d4c6c174cf98dfbff051a6d406f
Bartek <bartlomiej.fr1@gmail.com>
2023-03-07 22:47:03 +0100

    add user and build app

--- google_maps/src/User.ts
+++ google_maps/src/User.ts
@@ -1,3 +1,23 @@
 import { faker } from '@faker-js/faker';
+import { Mapable } from './CustomMap';
 
-export class User {}
+export class User implements Mapable {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+    color: string = 'red';
+
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude()),
+        };
+    }
+
+    markerContent(): string {
+        return `User Name: ${this.name}`;
+    }
+}
--- google_maps/src/index.ts
+++ google_maps/src/index.ts
@@ -4,4 +4,7 @@ import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
+const user = new User();
 const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
